eca56bea6b25fc41b776288674da7123bcb10f71
jamshaidkhan065 <dev.jamshaidkhan065@gmail.com>
2024-11-27 15:54:08 +0500

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../navbar/Navbar";
 
 const Layout = ({children}) => {
     return (
-        <div>
+        <div className="container mx-auto">
             <Navbar/>
             <div className="main-content min-h-screen">
                 {children}
